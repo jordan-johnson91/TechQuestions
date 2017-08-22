e72e5063445ee6577b1e848a5805280e1d67ce2d
Jordan Johnson <jordan@wecancodeit.org>
2017-08-22 18:14:17 -0400

    First Commit

---
+++ TechQuestions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            if(userNumber % 3 == 0 && userNumber % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if(userNumber % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+
+            else if(userNumber % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine("This number is not divisible by 3 or 5" );
+            }
+        }
+    }
+}
